f273f8244b107e75bc7a1d3991fb87d9c6ff34b4
Yakini <yakinilatha09@gmail.com>
2020-09-21 13:34:00 -0600

    Fixed the issue on new workout update

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,6 +14,19 @@ module.exports = function(app) {
        });
    });
 
+   app.put("/api/workouts/:id", ({body, params}, res) => {
+     console.log("ParamsId" +params.id);
+    db.workout.findOneAndUpdate({_id: params.id} , { $push: { exercises: body } },  { useFindAndModify: false, new: true , runValidators: true })
+       .then(workouts => {
+         console.log("Updated successfully" +workouts);
+ 
+         res.json(workouts);
+       })
+       .catch(err => {
+         res.json(err);
+       });
+   });
+   
   app.get("/api/workouts/", (req, res) => {
     db.workout.find()
       .then(workouts => {
@@ -23,28 +36,5 @@ module.exports = function(app) {
         res.status(400).json(err);
       });
   });
-
-//   app.put("/api/workouts/", ({ body }, res) => {
-//     db.workout.create(body)
-//       .then(dbTransaction => {
-//         res.json(dbTransaction);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-//   });
-  app.put("/api/workouts/:id", ({body, params}, res) => {
-   db.workout.findOneAndUpdate(params.id, { $push: { exercises: body } },  { new: true , runValidators: true })
-      .then(workouts => {
-        console.log("Updated successfully" +workouts);
-
-        res.json(workouts);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-
 }
   
